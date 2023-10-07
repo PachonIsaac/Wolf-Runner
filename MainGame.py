719c6495f6127aae1d92d157c2e8a709ebd9b1b0
IsaacP <isaac@pachon.com>
2023-10-07 16:21:10 -0500

    Ventana de inicio y boton de PLAY

--- MainGame.py
+++ MainGame.py
@@ -15,7 +15,16 @@ screen = pygame.display.set_mode(Initial_Size)
 clock = pygame.time.Clock()
 done= False
 
-Start_Background = os.path.join(current_directory, "Assets/Background/Start.jpeg")
+#Load Images 
+Start_Background = os.path.join(current_directory, "Assets/Background/Start.png")
+Button_Play = os.path.join(current_directory, "Assets/Other/Button Play.png")
+Button_Play_Iluminated = os.path.join(current_directory, "Assets/Other/Button Play Iluminated.png")
+
+# status of button PLAY
+button_play = False
+
+# status of the game
+in_game = False
 
 while not done:
     for event in pygame.event.get():
@@ -24,11 +33,31 @@ while not done:
         elif event.type == pygame.VIDEORESIZE:
             new_size = event.size
             screen = pygame.display.set_mode(new_size, pygame.RESIZABLE)
+        elif event.type == pygame.MOUSEMOTION:
+            mouse_pos = event.pos
+            if mouse_pos[0] >= 280 and mouse_pos[0] <= 430 and mouse_pos[1] >= 540 and mouse_pos[1] <= 590:
+                button_play = True
+            else:
+                button_play = False
+        elif event.type == pygame.MOUSEBUTTONDOWN and button_play:
+            in_game = True
+            
 
     # Image of background (escalated to the size of the screen)
     background_image = pygame.image.load(Start_Background).convert()
     background_image = pygame.transform.scale(background_image, screen.get_size())
     screen.blit(background_image, (0, 0))
+
+    if not in_game:
+        if button_play:
+            screen.blit(pygame.image.load(Button_Play_Iluminated).convert_alpha(), (280, 530))
+        else:
+            screen.blit(pygame.image.load(Button_Play).convert_alpha(), (280, 530))
+    else:
+        print("In Game") #Solo por llenar el else
+        # Lógica del juego aquí
+        # Puedes agregar toda la lógica del juego dentro de este bloque
+    
     
     # Actualizate the screen
     pygame.display.flip()
